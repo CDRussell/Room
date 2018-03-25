5745fda846efcd16852e9eb5f9465f77534f98ed
Craig Russell <craig.russell.1986@gmail.com>
2018-03-26 00:59:19 +0100

    Add @Delete function for deleting tasks

--- app/src/main/java/com/cdrussell/casterio/room/TaskDao.kt
+++ app/src/main/java/com/cdrussell/casterio/room/TaskDao.kt
@@ -2,6 +2,7 @@ package com.cdrussell.casterio.room
 
 import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
 
@@ -19,4 +20,7 @@ interface TaskDao {
 
     @Query("SELECT * FROM Task WHERE id = :taskId")
     fun getTask(taskId: Int): LiveData<Task>
+
+    @Delete
+    fun delete(task: Task)
 }
--- app/src/main/java/com/cdrussell/casterio/room/TaskDetailsActivity.kt
+++ app/src/main/java/com/cdrussell/casterio/room/TaskDetailsActivity.kt
@@ -7,13 +7,15 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_task_details.taskTitle
+import kotlin.concurrent.thread
 import kotlinx.android.synthetic.main.activity_task_details.taskId as taskIdInput
 
 class TaskDetailsActivity : AppCompatActivity() {
 
     private lateinit var taskDao: TaskDao
+
+    private lateinit var task: Task
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +25,13 @@ class TaskDetailsActivity : AppCompatActivity() {
         val taskId = extractTaskId()
         taskDao.getTask(taskId).observe(this, Observer<Task> {
             if (it == null) {
-                Toast.makeText(applicationContext, R.string.could_not_load_task, Toast.LENGTH_LONG).show()
                 finish()
                 return@Observer
             }
 
             taskTitle.text = it.title
             taskIdInput.text = it.id.toString()
+            task = it
         })
     }
 
@@ -48,7 +50,9 @@ class TaskDetailsActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_item_delete_task -> {
-                // need to implement task deletion here
+
+                thread { taskDao.delete(task) }
+
                 true
             }
             else -> super.onOptionsItemSelected(item)
