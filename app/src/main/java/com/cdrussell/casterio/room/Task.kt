5db61b0a6eac1f8ee02f220416847b5b08e2930c
Craig Russell <craig.russell.1986@gmail.com>
2018-04-07 04:01:32 +0100

    Add foreign key definition

--- app/src/main/java/com/cdrussell/casterio/room/Task.kt
+++ app/src/main/java/com/cdrussell/casterio/room/Task.kt
@@ -1,11 +1,20 @@
 package com.cdrussell.casterio.room
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
+import com.cdrussell.casterio.room.users.User
 
 
-@Entity
+@Entity(
+    foreignKeys = [(ForeignKey(
+        entity = User::class,
+        parentColumns = ["id"],
+        childColumns = ["userId"],
+        onDelete = ForeignKey.SET_NULL
+    ))]
+)
 data class Task(
     @PrimaryKey(autoGenerate = true) var id: Int,
     var title: String,
